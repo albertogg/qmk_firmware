2a8d776525829296f3a74b82db0ac18da832c808
Alberto Grespan <alberto@albertogrespan.com>
2020-02-08 22:02:25 -0400

    Move the FUNC layout to tab and Ctrl with caps
    Using the LT layer switching to change to the FUNC layout on the tab key
    and the Caps lock with Ctrl.
    
    Fix
    
    Fix the caps key to work as ctrl when held

--- keyboards/whitefox/keymaps/albertogg/keymap.c
+++ keyboards/whitefox/keymaps/albertogg/keymap.c
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,---------------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| Backsp|Ins|
      * |---------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|Del|
+     * |Fn|Tb|  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|Del|
      * |---------------------------------------------------------------|
-     * |Fn|Cps|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
+     * |Ctl|Cp|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
      * |---------------------------------------------------------------|
      * |Shif    |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |PgD|
      * |---------------------------------------------------------------|
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_vanilla( \
-        KC_GESC,          KC_1,     KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL, KC_BSPC,KC_INS, \
-        KC_TAB,           KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,KC_BSLS,KC_DEL, \
-        LT(_FUNC,KC_CAPS),KC_A,     KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,           KC_ENT, KC_PGUP, \
-        KC_LSFT,          KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,           KC_UP,  KC_PGDN, \
-        KC_LCTL,          KC_LALT,  KC_LGUI,               KC_SPC,                          KC_RGUI,KC_RALT,MO(_MEDIA),KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_GESC,            KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,   KC_EQL, KC_BSPC,KC_INS, \
+        LT(_FUNC,KC_TAB),   KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,   KC_RBRC,KC_BSLS,KC_DEL, \
+        LCTL_T(KC_CAPS),    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,           KC_ENT, KC_PGUP, \
+        KC_LSFT,            KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,           KC_UP,  KC_PGDN, \
+        KC_LCTL,            KC_LALT, KC_LGUI,               KC_SPC,                          KC_RGUI,KC_RALT,MO(_MEDIA),KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Layer 1: Func Layer
      * ,---------------------------------------------------------------.
